034ac8d1f9b0b666075a028b54ef75a3024cac9e
Aronnord <144971443+Aronnord@users.noreply.github.com>
2023-11-19 17:20:14 +0000

    Add files via upload

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import 'package:advance_lessons/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    ' I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?',
+    [
+      'An echo',
+      'Face',
+      'Sound',
+      'Wall',
+    ],
+  ),
+  QuizQuestion('The more you take, the more you leave behind. What am I?', [
+    'Footsteps',
+    'Money',
+    'Love',
+    'Food',
+  ]),
+  QuizQuestion(
+    'I am light as a feather, yet the strongest person can not hold me for much longer than a minute. What am I?',
+    [
+      'Breath',
+      'Rock',
+      'Weights',
+      'Air',
+    ],
+  ),
+  QuizQuestion(
+    'What comes once in a minute, twice in a moment, but never in a thousand years?',
+    [
+      'The letter M',
+      'Time',
+      'Number',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    ' am taken from a mine and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?',
+    [
+      'Pencil lead/graphite',
+      'Coal',
+      'Corpse',
+      'Nothing',
+    ],
+  ),
+  QuizQuestion(
+    'I am full of keys, but I can not open any locks. What am I?',
+    [
+      'A keyboard',
+      'Keyring',
+      'Pocket',
+      'Programming',
+    ],
+  ),
+];
